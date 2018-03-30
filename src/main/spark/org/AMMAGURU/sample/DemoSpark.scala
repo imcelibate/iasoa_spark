8c5e2a4f3301a291d7ee7699e51835db1f3f3e0f
imcelibate <amar.rudran@gmail.com>
2018-03-29 23:13:29 -0400

    Update in test

--- src/main/spark/org/AMMAGURU/sample/DemoSpark.scala
+++ src/main/spark/org/AMMAGURU/sample/DemoSpark.scala
@@ -38,16 +38,31 @@ object DemoSpark {
     
      //Flat Map test
     val lineitem = ipText.flatMap(line => line.split(" "))
+    println("Flat Map TEst : ")
     lineitem.foreach(println)
     
-     //Filter test - incomplete
-    println("AMMA :  even numbers")
-    val lineItemEven = lineitem.flatMap( eachItem => {
-                    eachItem.filter(value => (value.toInt)%2 == 0)
-      
-                }
-        )
-    lineItemEven.foreach(println)
+    println("AMMA : Filter even numbers")
+    val lineItemFilter = lineitem.flatMap( eachItem => eachItem.split("\t").filter(value => (value.toInt)%2 == 0))
+    println("RDD Count: "+lineItemFilter.count())
+    lineItemFilter.foreach(println)
+    
+    //Count by Value test
+    println("AMMA : Count by Value test")
+    val countByValueTest = lineItemFilter.countByValue();
+    countByValueTest.foreach(println)
+    
+    
+    
+//     //Filter test - incomplete
+//    println("AMMA :  even numbers")
+//    val lineItemEven = lineitem.flatMap( eachItem => {
+//                    eachItem.filter(value => (value.toInt)%2 == 0)
+//      
+//                }
+//        )
+//    lineItemEven.foreach(println)
+    
+
     
     //Map Partition
     val lineitem1 = ipText.mapPartitions{
@@ -90,6 +105,30 @@ object DemoSpark {
     val rdd2 = sparkContext.parallelize(Seq((1,"jan",2016),(3,"nov",2014),(16,"feb",2014),(3,"nov",2014)))
     val result1 = rdd2.distinct()
     println(result1.collect().mkString(", "))
+ 
+    //groupByKey rdd test  
+    println("groupByKey Test")
+    val data1 = sparkContext.parallelize(Array(('k',5),('s',3),('s',4),('p',7),('p',5),('t',8),('k',6)))
+    val group = data1.groupByKey()
+    group.foreach(println)
+    
+    //collect Action test
+    println("collect Action Test")
+    val lineitem3 = ipText.flatMap(line => line.split(" "))  
+    val lineItemArray = lineitem3.collect()
+    println("Size in Array : "+ lineItemArray.size)
+    println("Item 1 : "+ lineItemArray(0))
+    println("Size after split  : "+ lineItemArray(0).toString().split("\t").size)  
+    lineItemArray.foreach(println)
+
+    //reduce action test
+    println("reduce Action Test")
+    val exArray = Array("AMMA", "KALI", "MATHA", "GURU", "AMMACHI", "PONAMMA","AMMA","AMMAGURU", "GURU")
+    val  wordCount = sparkContext.parallelize(exArray)
+    val wordcount1 =  wordCount.map(word => (word,1))
+    val wordcount2 = wordcount1.reduceByKey(_+_)
+        wordcount2.foreach(println)
+    
     
   }
 
